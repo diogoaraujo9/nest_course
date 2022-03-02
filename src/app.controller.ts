789d66a6ecb861c4ca5de2ed1116432295d1fb72
Diogo Araujo <diogo_araujo9@hotmail.com>
2022-03-01 21:34:16 -0300

    feat: criação do crud de tweets

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('app')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/helloworld')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/forecast')
+  getForecast(): string {
+    return 'rain';
   }
 }
